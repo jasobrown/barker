0eb0458664bb2578ae41b5d0e65c7430dfed274a
Jason Brown <jasedbrown@gmail.com>
2015-03-16 15:20:42 -0700

    add new benchmark for querying System.nanoTime()

---
+++ src/main/java/jmh/barker/SystemNanoTime.java
@@ -0,0 +1,28 @@
+package jmh.barker;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@State(Scope.Thread)
+@Warmup(iterations = 4, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 8, time = 2, timeUnit = TimeUnit.SECONDS)
+@Fork(4)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.SampleTime)
+public class SystemNanoTime
+{
+    @Benchmark
+    public long standardLoop()
+    {
+        return System.nanoTime();
+    }
+}
